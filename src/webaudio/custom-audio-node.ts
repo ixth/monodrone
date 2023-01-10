9878b08a986b1473d742ab01e5d9538a2e654d5c
Mikhail Menshikov <ainop@ya.ru>
2023-01-10 22:36:58 +0100

    Refactor WebAudio components

--- src/webaudio/custom-audio-node.ts
+++ src/webaudio/custom-audio-node.ts
@@ -1,10 +1,69 @@
-/* eslint-disable */
-export abstract class CustomAudioNode {
-    protected constructor(protected context: BaseAudioContext) {}
+export class CustomAudioNode extends EventTarget implements AudioNode {
+    channelCount = 0;
 
-    abstract __connectFrom(sourceNode: AudioNode, output?: number, input?: number): AudioNode;
+    channelCountMode: ChannelCountMode = 'max';
 
-    abstract __disconnectFrom(sourceNode: AudioNode, output?: number, input?: number): void;
+    channelInterpretation: ChannelInterpretation = 'speakers';
+
+    readonly numberOfInputs = 0;
+
+    readonly numberOfOutputs = 0;
+
+    protected readonly _inputs: AudioNode[] = [];
+
+    protected readonly _outputs: AudioNode[] = [];
+
+    constructor(readonly context: BaseAudioContext) {
+        super();
+    }
+
+    __connectFrom(sourceNode: AudioNode, output = 0, input = 0): AudioNode {
+        sourceNode.connect(this._inputs[input], output);
+        return this;
+    }
+
+    __disconnectFrom(sourceNode: AudioNode, output = 0, input = 0): void {
+        sourceNode.disconnect(this._inputs[input], output);
+    }
+
+    connect(destinationNode: AudioNode, output?: number, input?: number): AudioNode;
+
+    connect(destinationParam: AudioParam, output?: number): void;
+
+    connect(destination: AudioNode | AudioParam, output = 0, input = 0): AudioNode | void {
+        if (destination instanceof AudioNode) {
+            return this._outputs[output].connect(destination, 0, input);
+        }
+        this._outputs[output].connect(destination);
+    }
+
+    disconnect(): void;
+
+    disconnect(output: number): void;
+
+    disconnect(destinationNode: AudioNode): void;
+
+    disconnect(destinationNode: AudioNode, output: number): void;
+
+    disconnect(destinationNode: AudioNode, output: number, input: number): void;
+
+    disconnect(destinationParam: AudioParam): void;
+
+    disconnect(destinationParam: AudioParam, output: number): void;
+
+    disconnect(destination?: number | AudioNode | AudioParam, output = 0, input = 0): void {
+        if (typeof destination === 'undefined') {
+            this._outputs[output].disconnect();
+        } else if (typeof destination === 'number') {
+            this._outputs[output].disconnect(destination);
+        } else if (destination instanceof AudioNode) {
+            return this._outputs[output].disconnect(destination, 0, input);
+        } else if (destination instanceof AudioParam) {
+            return this._outputs[output].disconnect(destination, 0);
+        } else {
+            this._outputs[output].disconnect(destination, 0, input);
+        }
+    }
 }
 
 export interface PatchedAudioNode extends AudioNode {
@@ -13,34 +72,38 @@ export interface PatchedAudioNode extends AudioNode {
 }
 
 export const patchAudioNode = (): void => {
-    if (typeof AudioNode !== 'undefined') {
-        Object.assign(AudioNode.prototype, {
-            originalConnect: AudioNode.prototype.connect,
-
-            connect: function audioNodeConnect(
-                this: PatchedAudioNode,
-                ...args: Parameters<AudioNode['connect']>
-            ): AudioNode | void {
-                const [destinationNode, ...rest] = args;
-                if (destinationNode instanceof CustomAudioNode) {
-                    return destinationNode.__connectFrom(this, ...rest);
-                }
-                this.originalConnect(...args);
-            },
-
-            originalDisconnect: AudioNode.prototype.disconnect,
-
-            disconnect: function audioNodeDisconnect(
-                this: PatchedAudioNode,
-                ...args: Parameters<AudioNode['disconnect']>
-            ): AudioNode | void {
-                const [destinationNode, ...rest] = args;
-                if (destinationNode instanceof CustomAudioNode) {
-                    destinationNode.__disconnectFrom(this, ...rest);
-                    return;
-                }
-                this.originalDisconnect(...args);
-            },
-        });
+    if (typeof AudioNode === 'undefined' || 'originalConnect' in AudioNode.prototype) {
+        return;
     }
+
+    Object.assign(AudioNode.prototype, {
+        /* eslint-disable-next-line @typescript-eslint/unbound-method */
+        originalConnect: AudioNode.prototype.connect,
+
+        connect: function audioNodeConnect(
+            this: PatchedAudioNode,
+            ...args: Parameters<AudioNode['connect']>
+        ): AudioNode | void {
+            const [destinationNode, ...rest] = args;
+            if (destinationNode instanceof CustomAudioNode) {
+                return destinationNode.__connectFrom(this, ...rest);
+            }
+            this.originalConnect(...args);
+        },
+
+        /* eslint-disable-next-line @typescript-eslint/unbound-method */
+        originalDisconnect: AudioNode.prototype.disconnect,
+
+        disconnect: function audioNodeDisconnect(
+            this: PatchedAudioNode,
+            ...args: Parameters<AudioNode['disconnect']>
+        ): AudioNode | void {
+            const [destinationNode, ...rest] = args;
+            if (destinationNode instanceof CustomAudioNode) {
+                destinationNode.__disconnectFrom(this, ...rest);
+                return;
+            }
+            this.originalDisconnect(...args);
+        },
+    });
 };
